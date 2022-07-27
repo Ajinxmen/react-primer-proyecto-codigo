98f9bc59e646ddc6503ad5a92bf4259624b7b7ae
Joseph Wilfredo Flores <Ajinxmen>
2022-07-26 22:11:48 -0500

    primer react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// Cuando importemos algo de react procuremos siempre
+// Ponerlo al principio
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Counter from "./components/Counter";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  //vamos a crear una funcion que permita incrementar el contador
+  const increment = () => {
+    //setCounter: Es la funciona que cambia el valor de counter
+    // y adentro indico que el nuevo valor es counter + 1
+    setCounter(counter + 1);
+  };
+
+  const decrease = () => setCounter(counter - 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Counter
+        contador={counter}
+        incrementar={increment}
+        decrementar={decrease}
+      />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h3>Contactos</h3>
+            <a href="mailto:ajinxmen@gmail.com">Correo</a>
+        </div>
+    );
+}
+
+export default Footer;
